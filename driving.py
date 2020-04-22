17b2bb5e402e0ce4217dcb3cb693ec5203214e2e
Benny <snk2kimo@gmail.com>
2020-04-22 16:39:28 +0800

    Add diveing檔案

---
+++ driving.py
@@ -0,0 +1,17 @@
+# 國家/開車對應年齡程式
+country = input('請輸入你的國家：')
+age = input('請輸入年齡：')
+age = int(age)
+
+if country == '台灣':
+	if age >= 18:
+		print('你可以考駕照摟!')
+	else:
+		print('你還未成年喔!')
+elif country == '美國':
+	if age >= 16:
+		print('你可以考駕照摟!')
+	else:
+		print('你還未成年喔!')
+else:
+	print('請輸入 台灣/美國')
